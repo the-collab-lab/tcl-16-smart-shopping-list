ce8d004a057b30f0c3002f410ad29df1b02b7258
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-05 21:40:40 -0500

    get items from database and renders only one item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { fb } from './lib/firebase';
 
 function App() {
   const [term, setTerm] = useState('');
+  const [list, setList] = useState([]);
 
   // initialize firestore
   const db = fb.firestore();
@@ -10,6 +11,34 @@ function App() {
   const onInputChange = (e) => {
     setTerm(e.target.value);
   };
+
+  const getItems = async () => {
+    const data = await db.collection('shoppingLists').get();
+    data.docs.map((doc) => setList([...list, doc.data()]));
+
+    // console.log(data)
+    // for(const doc of data.docs){
+    //   console.log(typeof doc.data().item);
+    // }
+    // return "Hi"
+    //return data.docs.map(doc => console.log(doc.data()))
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  //   async getMarker() {
+  //     const snapshot = await firebase.firestore().collection('events').get()
+  //     return snapshot.docs.map(doc => doc.data());
+  // }
+
+  const renderList = () => {
+    console.log(Array.isArray(list));
+    return list.map((item) => <li>{item.item}</li>);
+  };
+
+  console.log(list);
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -20,12 +49,17 @@ function App() {
     setTerm('');
   };
 
+  //getItems()
+
   return (
     <div className="App">
       <h1>firestore connect</h1>
       <form onSubmit={onSubmitHandler}>
         <input value={term} onChange={onInputChange} />
       </form>
+      <div>
+        <ul>{renderList()}</ul>
+      </div>
     </div>
   );
 }
