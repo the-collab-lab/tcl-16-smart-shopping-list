4e13e68dbea1edb9f7c0b838daadea2413cdd179
Kevin <thekidnamedkd@gmail.com>
2021-01-07 13:34:28 -0800

    add links to app with current route displayed in url + dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import logo from './tcl-logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>TCL-16</h1>
-        <p>
-          Welcome to TCL-16! This app component is a good starting place for the
-          Smart Shopping List! WEEK 1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>TCL-16</h1>
+          <p>
+            Welcome to TCL-16! This app component is a good starting place for
+            the Smart Shopping List!
+          </p>
+          <h2>Week 1</h2>
+
+          <MenuLink activeWhenExact={true} to="/ListView" label="List View" />
+          <MenuLink to="/AddItem" label="Add Item" />
+
+          <Switch>
+            <Route exact path="/ListView">
+              <ListView />
+            </Route>
+            <Route exact path="/AddItem">
+              <AddItem />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+function MenuLink({ label, to, activeWhenExact }) {
+  let match = useRouteMatch({
+    path: to,
+    exact: activeWhenExact,
+  });
+
+  return (
+    <div className={match ? 'active' : ''}>
+      <Link to={to}>{label}</Link>
+    </div>
+  );
+}
+
+function ListView() {
+  return (
+    <div>
+      <h3>List View</h3>
+    </div>
+  );
+}
+
+function AddItem() {
+  return (
+    <div>
+      <h3>Add Item</h3>
     </div>
   );
 }
