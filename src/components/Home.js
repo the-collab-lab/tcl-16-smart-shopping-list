0537ac1e775f16145c08726ede0e72394e2bbfa5
Maeesha <maeesha.rahman@gmail.com>
2021-01-19 15:23:17 -0500

    added form and inputs related to join shopping list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import getToken from '../lib/tokens.js';
 import { useHistory } from 'react-router-dom';
+import { db } from '../lib/firebase';
 
 const Home = (props) => {
+  const [token, setToken] = useState('');
+
   const history = useHistory();
 
   const generateToken = () => {
@@ -12,12 +15,37 @@ const Home = (props) => {
     history.push('/ListView');
   };
 
+  const onTokenInputChange = (e) => {
+    setToken(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    db.collection(localStorage.getItem('token')).add({});
+
+    history.push('/ListView');
+  };
+
   return (
     <div className="home">
       <h1>Welcome to Smart Shopping List!</h1>
       <button className="new-list-btn" onClick={generateToken}>
         Create a new list
       </button>
+      <p>- or -</p>
+      <p>Join an existing shopping list by entering a three word token.</p>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="joinList">Share token </label>
+        <input
+          id="joinList"
+          name="joinList"
+          type="text"
+          placeholder="three word token"
+          value={token}
+          onChange={onTokenInputChange}
+        />
+        <button>Join an existing list</button>
+      </form>
     </div>
   );
 };
