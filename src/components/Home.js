b29a226471348e27617e5d0abf5290397a5ee047
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-22 19:29:48 -0500

    Convert all single quotes to double

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import getToken from '../lib/tokens.js';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import getToken from "../lib/tokens.js";
+import { useHistory } from "react-router-dom";
 
 const Home = (props) => {
   const history = useHistory();
 
   const generateToken = () => {
     const token = getToken();
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
     props.setAuth(token);
-    history.push('/ListView');
+    history.push("/ListView");
   };
 
   return (
