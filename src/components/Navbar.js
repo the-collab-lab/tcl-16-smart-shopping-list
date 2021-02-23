8ed40693bcfcc9bca0149f6a5258850e75632619
Kevin <thekidnamedkd@gmail.com>
2021-02-22 21:29:50 -0800

    port local styling to sass sheets for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { ReactComponent as MCB } from "../img/meat_cheese_bread.svg";
 import { ReactComponent as ListView } from "../img/list_view.svg";
 import { ReactComponent as AddItem } from "../img/add_item.svg";
@@ -7,42 +8,24 @@ import { ReactComponent as ChangeList } from "../img/change_list.svg";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const navContainer = {
-    display: "flex",
-    flexDirection: "column",
-    alignContent: "center",
-    alignItems: "center",
-  };
-
-  const navStyle = {
-    display: "flex",
-    alignItems: "center",
-    alignContent: "space-between",
-    textAlign: "center",
-  };
-
-  const link = {
-    marginLeft: "10%",
-  };
-
   return (
     <>
-      <div style={navContainer}>
-        <div style={navStyle}>
+      <div className="navContainer">
+        <div className="navStyle">
           <MCB />
           <h1>MCB</h1>
-          <div style={link}>
+          <div className="navLink">
             <Link to="/List">
               <ListView />
             </Link>
           </div>
-          <div style={link}>
+          <div className="navLink">
             <Link to="/AddItem">
               <AddItem />
             </Link>
           </div>
-          <div style={link}>
-            <Link to="/Home">
+          <div className="navLink">
+            <Link to="/">
               <ChangeList />
             </Link>
           </div>
