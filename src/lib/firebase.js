8e14bd2d92b47b55e8d9108de862afa17aa79fec
Waverley Leung <waverley.nyc@gmail.com>
2021-04-01 16:58:01 -0400

    update environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,13 +5,13 @@ import "firebase/firestore";
 
 // Initalize Firebase.
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
